51bdf49476f8432bfee6c832140b916cd493aafd
John Comeau <jc@unternet.net>
2020-04-01 17:47:19 -0700

    got the first part working

--- filter.py
+++ filter.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 'https://stackoverflow.com/a/45044199/493161'
 def response(flow):
-    print("")
-    print("="*50)
-    #print("FOR: " + flow.request.url)
-    print(flow.request.method + " " + flow.request.path + " " + flow.request.http_version)
-
-    print("-"*50 + "request headers:")
-    for k, v in flow.request.headers.items():
-        print("%-20s: %s" % (k.upper(), v))
-
-    print("-"*50 + "response headers:")
-    for k, v in flow.response.headers.items():
-        print("%-20s: %s" % (k.upper(), v))
-        print("-"*50 + "request headers:")
+    '''
+    Log response to a unique log for these host-port combinations
+    '''
+    fromaddr = flow.client_conn.address
+    fromhost = fromaddr[0]
+    fromport = str(fromaddr[1])
+    tohost = flow.request.host
+    toport = str(flow.request.port)
+    logname = '.'.join((fromhost, fromport, tohost, toport)) + '.log'
+    with open(f'/var/log/mitmproxy/{logname}', 'a') as logfile:
+        print(f'Request from {fromhost}:{fromport} to'
+              f' {tohost}:{toport}', file=logfile)
