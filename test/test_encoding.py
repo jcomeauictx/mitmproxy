98ba18af27880699ace0672359877c71fb5ea0e9
John Comeau <jc@unternet.net>
2026-04-07 01:04:09 +0000

    fixed test_encoding errors (FAILs)

--- test/test_encoding.py
+++ test/test_encoding.py
@@ -1,19 +1,19 @@
 from libmproxy import encoding
 
 def test_identity():
-    assert "string" == encoding.decode("identity", b'string')
-    assert "string" == encoding.encode("identity", b'string')
-    assert not encoding.encode("nonexistent", b'string')
-    assert None == encoding.decode("nonexistent encoding", b'string')
+    assert b'string' == encoding.decode('identity', b'string')
+    assert b'string' == encoding.encode('identity', b'string')
+    assert not encoding.encode('nonexistent', b'string')
+    assert None == encoding.decode('nonexistent encoding', b'string')
 
 
 def test_gzip():
-    assert "string" == encoding.decode("gzip", encoding.encode("gzip", b'string'))
-    assert None == encoding.decode("gzip", b'bogus')
+    assert b'string' == encoding.decode('gzip', encoding.encode('gzip', b'string'))
+    assert None == encoding.decode('gzip', b'bogus')
 
 
 def test_deflate():
-    assert "string" == encoding.decode("deflate", encoding.encode("deflate", b'string'))
-    assert "string" == encoding.decode("deflate", encoding.encode("deflate", b'string')[2:-4])
-    assert None == encoding.decode("deflate", b'bogus')
+    assert b'string' == encoding.decode('deflate', encoding.encode('deflate', b'string'))
+    assert b'string' == encoding.decode('deflate', encoding.encode('deflate', b'string')[2:-4])
+    assert None == encoding.decode('deflate', b'bogus')
 
